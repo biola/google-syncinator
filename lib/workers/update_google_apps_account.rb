4010eb28a11fb1f6b715b3432283cd185045e5b0
Adam Crownoble <adam.crownoble@biola.edu>
2015-04-06 13:27:35 -0700

    Fix reporting :create to trogdir on update

--- lib/workers/update_google_apps_account.rb
+++ lib/workers/update_google_apps_account.rb
@@ -7,7 +7,7 @@ module Workers
     def perform(email, first_name, last_name, department, title, privacy, sync_log_id)
       begin
         if GoogleAccount.new(email).update!(first_name, last_name, department, title, privacy)
-          TrogdirChangeFinishWorker.perform_async sync_log_id, :create
+          TrogdirChangeFinishWorker.perform_async sync_log_id, :update
         end
       rescue StandardError => err
         TrogdirChangeErrorWorker.perform_async sync_log_id, err.message
