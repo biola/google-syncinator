d24556b0092ecc5ce41e9910e8f484639fecb474
Adam Crownoble <adam.crownoble@biola.edu>
2015-04-24 08:40:44 -0700

    Fix "action is invalid error"
    Make JoinGoogleGroup and LeaveGoogleGroup return an action symboli

--- lib/service_objects/join_google_group.rb
+++ lib/service_objects/join_google_group.rb
@@ -1,9 +1,11 @@
 module ServiceObjects
   class JoinGoogleGroup < Base
     def call
-      Whitelist.filter(change.joined_groups).each do |group|
+      changes = Whitelist.filter(change.joined_groups).each do |group|
         google_account.join! group
       end
+
+      changes.any? ? :update : :skip
     end
 
     def ignore?
