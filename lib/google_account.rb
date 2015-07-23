18f9d5bf355ceb5f70f42315568683a5ed6c4891
Adam Crownoble <adam@obledesign.com>
2015-07-23 17:42:24 +0800

    Add GoogleAccount#never_active and #inactive
    Which gets a list of emails that never logged in or have not logged
    in in a long time.

--- lib/google_account.rb
+++ lib/google_account.rb
@@ -1,7 +1,9 @@
 class GoogleAccount
-  attr_reader :email
-
   class GoogleAppsAPIError < RuntimeError; end
+
+  ZERO_DATE = '1970-01-01T00:00:00.000Z'
+
+  attr_reader :email
 
   def initialize(email)
     @email = email
@@ -124,6 +126,69 @@ class GoogleAccount
     GoogleAccount.full_email(email)
   end
 
+  def self.never_active
+    page_token = nil
+    never_active_emails = []
+
+    loop do
+      result = api.execute(
+        api_method: reports.user_usage_report.get,
+        parameters: {
+          userKey: 'all',
+          date: 3.days.ago.to_date.to_s, # TODO: grab from settings
+          filters: "accounts:last_login_time==#{ZERO_DATE},accounts:is_disabled==false",
+          parameters: 'accounts:last_login_time',
+          fields: 'nextPageToken,usageReports(entity/userEmail,parameters)',
+          pageToken: page_token
+        }
+      )
+
+      raise GoogleAppsAPIError, result.error_message unless result.success?
+
+      result.data.usage_reports.each do |report|
+        never_active_emails << report.entity.user_email
+      end
+
+      break unless page_token = result.next_page_token
+    end
+
+    never_active_emails
+  end
+
+  def self.inactive
+    page_token = nil
+    inactive_emails = []
+
+    loop do
+      result = api.execute(
+        api_method: reports.user_usage_report.get,
+        parameters: {
+          userKey: 'all',
+          date: 3.days.ago.to_date.to_s, # TODO: grab from settings
+          filters: "accounts:last_login_time<#{1.year.ago.iso8601},accounts:last_login_time>#{ZERO_DATE},accounts:is_disabled==false",
+          parameters: 'accounts:last_login_time',
+          fields: 'nextPageToken,usageReports(entity/userEmail,parameters)',
+          pageToken: page_token
+        }
+      )
+
+      raise GoogleAppsAPIError, result.error_message unless result.success?
+
+      result.data.usage_reports.each do |report|
+        # TODO: get time from Settings
+        last_login = report.parameters.find{|p| p.name = 'accounts:last_login_time'}.datetime_value
+        # It shouldn't be necessary to recheck the last_login here but better safe than sorry
+        if last_login < 1.year.ago
+          inactive_emails << report.entity.user_email
+        end
+      end
+
+      break unless page_token = result.next_page_token
+    end
+
+    inactive_emails
+  end
+
   def self.full_email(email)
     if email.include? '@'
       email
@@ -133,7 +198,7 @@ class GoogleAccount
   end
 
   def self.group_to_email(group_name)
-    full_email(group_name.to_s.downcase.gsub /[^a-z0-9]+/, '.')
+    full_email(group_name.to_s.downcase.gsub(/[^a-z0-9]+/, '.'))
   end
 
   def self.random_password
@@ -151,24 +216,40 @@ class GoogleAccount
     true
   end
 
-  def api
-    return @api unless @api.nil?
-
-    @api = Google::APIClient.new(
+  def self.api
+    api = Google::APIClient.new(
       application_name: Settings.google.api_client.application_name,
       application_version: Settings.google.api_client.application_version)
 
     key = Google::APIClient::KeyUtils.load_from_pkcs12(Settings.google.api_client.key_path, Settings.google.api_client.secret)
 
-    @api.authorization = Signet::OAuth2::Client.new(
+    api.authorization = Signet::OAuth2::Client.new(
       token_credential_uri: 'https://accounts.google.com/o/oauth2/token',
       audience: 'https://accounts.google.com/o/oauth2/token',
       scope: Settings.google.api_client.scopes,
       issuer: Settings.google.api_client.issuer,
       signing_key: key)
-    @api.authorization.person = Settings.google.api_client.person
-    @api.authorization.fetch_access_token!
-    @api
+    api.authorization.person = Settings.google.api_client.person
+    api.authorization.fetch_access_token!
+    api
+  end
+
+  def api
+    return @api unless @api.nil?
+
+    @api = self.class.api
+  end
+
+  def self.reports
+    api.discovered_api('admin', 'reports_v1')
+  end
+
+  def self.directory
+    api.discovered_api('admin', 'directory_v1')
+  end
+
+  def directory
+    @directory ||= self.class.directory
   end
 
   def data
@@ -181,9 +262,5 @@ class GoogleAccount
 
       result.success? ? result.data : nil
     )
-  end
-
-  def directory
-    @directory ||= api.discovered_api('admin', 'directory_v1')
   end
 end
