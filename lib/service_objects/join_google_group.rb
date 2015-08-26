03e5af4e2a2a38d09064865bd419eebadbc8731b
Michael Stephens <michael.stephens@biola.edu>
2015-08-26 10:59:06 -0700

    Make the variable changed exist
    If the person never joins/leaves a group, the varialbe changed would not
    exist. This way it will be nil and thus trigger a false.

--- lib/service_objects/join_google_group.rb
+++ lib/service_objects/join_google_group.rb
@@ -2,10 +2,10 @@ module ServiceObjects
   class JoinGoogleGroup < Base
     def call
       changes = Whitelist.filter(change.joined_groups).each do |group|
-        changed = true if google_account.join! group
+        @changed = true if google_account.join! group
       end
 
-      changes.any? && changed ? :update : :skip
+      changes.any? && @changed ? :update : :skip
     end
 
     def ignore?
--- lib/service_objects/leave_google_group.rb
+++ lib/service_objects/leave_google_group.rb
@@ -2,10 +2,10 @@ module ServiceObjects
   class LeaveGoogleGroup < Base
     def call
       changes = Whitelist.filter(change.left_groups).each do |group|
-        changed = true if google_account.leave! group
+        @changed = true if google_account.leave! group
       end
 
-      changes.any? && changed ? :update : :skip
+      changes.any? && @changed ? :update : :skip
     end
 
     def ignore?
