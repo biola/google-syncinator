c5ae74e64898c5309d05569ec17fb3b81acc7f05
Adam Crownoble <adam@obledesign.com>
2015-09-09 17:15:54 +0800

    Don't need to call cancel_deprovisioning! twice

--- lib/service_objects/reprovision_google_account.rb
+++ lib/service_objects/reprovision_google_account.rb
@@ -4,8 +4,6 @@ module ServiceObjects
     # Reactivate a suspended or deleted account that used to belong to the user
     # @return [:create]
     def call
-      reprovisionable_email.cancel_deprovisioning!
-
       # We'll delay the worker just a few seconds to prevent race conditions
       Workers::ScheduleActions.perform_async(reprovisionable_email.id.to_s, 10, :activate)
 
