5a27ec7c0b69bedcc891f4d306537980d2afdc76
Michael Stephens <michael.stephens@biola.edu>
2015-09-11 10:04:22 -0700

    When a user's biola_id updates, it should update
    in the WS Legacy Email Table.

--- lib/service_objects/update_email_address.rb
+++ lib/service_objects/update_email_address.rb
@@ -3,13 +3,17 @@ module ServiceObjects
     def call
       # The ID hash does not come through with the hash of the person ids
       # So we have to make a work around for it.
-      biola_id = TrogdirPerson.new(change.person_uuid).biola_id
-      UpdateLegacyEmailTable.new(change).insert_and_update(biola_id, change.old_university_email, change.new_university_email)
+      if change.biola_id_updated?
+        UpdateLegacyEmailTable.new(change).insert_and_update_id(change.old_biola_id, change.new_biola_id)
+      else
+        biola_id = TrogdirPerson.new(change.person_uuid).biola_id
+        UpdateLegacyEmailTable.new(change).insert_and_update_address(biola_id, change.old_university_email, change.new_university_email)
+      end
       :update
     end
 
     def ignore?
-      !change.university_email_updated?
+      !(change.university_email_updated? || change.biola_id_updated?)
     end
   end
 end
