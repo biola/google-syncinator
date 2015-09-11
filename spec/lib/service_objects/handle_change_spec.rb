6cc50e0adb0b2754434f6e3b263820aa9069d45f
Michael Stephens <michael.stephens@biola.edu>
2015-09-11 10:19:01 -0700

    Add test for updating Legacy email biola_id

--- spec/lib/service_objects/handle_change_spec.rb
+++ spec/lib/service_objects/handle_change_spec.rb
@@ -44,6 +44,19 @@ describe ServiceObjects::HandleChange do
     end
   end
 
+  context 'when biola id updated' do
+    let(:change_hash) { JSON.parse(File.read('./spec/fixtures/update_biola_id.json')) }
+
+    it 'calls UpdateEmailAddress' do
+      expect_any_instance_of(ServiceObjects::AssignEmailAddress).to_not receive(:call)
+      expect_any_instance_of(ServiceObjects::UpdateEmailAddress).to receive(:call).and_return(:create)
+      expect(Workers::ChangeFinish).to receive(:perform_async).with(kind_of(String), :create)
+      expect(Workers::ChangeError).to_not receive(:perform_async)
+
+      subject.call
+    end
+  end
+
   context 'when account info updated' do
     let(:change_hash) { JSON.parse(File.read('./spec/fixtures/update_person.json')) }
 
