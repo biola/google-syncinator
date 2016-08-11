a91a3891b14f249170d8b9ad2e0c820e45b6a5aa
Devin Hanlon <devin.hanlon@biola.edu>
2016-08-11 16:09:12 -0700

    Updated specs to include named params.

--- spec/lib/workers/update_person_email_spec.rb
+++ spec/lib/workers/update_person_email_spec.rb
@@ -24,41 +24,41 @@ describe Workers::UpdatePersonEmail do
 
   context 'a new email' do
     it 'updates the person email' do
-      expect { subject.new(email.id, new_uuid, new_address, first_name, last_name, password, vfe, privacy).perform }.to change { email.reload.address }.from(old_address).to new_address
+      expect { subject.new(id: email.id, uuid: new_uuid, address: new_address, first_name: first_name, last_name: last_name, password: password, vfe: vfe, privacy: privacy).perform }.to change { email.reload.address }.from(old_address).to new_address
     end
 
     it 'updates the trogdir email' do
-      expect { subject.new(email.id, old_uuid, new_address, first_name, last_name, password, vfe, privacy).perform}.to change(Workers::Trogdir::RenameEmail.jobs, :size).by(1)
+      expect { subject.new(id: email.id, uuid: old_uuid, address: new_address, first_name: first_name, last_name: last_name, password: password, vfe: vfe, privacy: privacy).perform}.to change(Workers::Trogdir::RenameEmail.jobs, :size).by(1)
     end
 
     it 'updates the legacy table' do
-      expect { subject.new(email.id, new_uuid, new_address, first_name, last_name, password, vfe, privacy).perform}.to change(Workers::LegacyEmailTable::Rename.jobs, :size).by(1)
+      expect { subject.new(id: email.id, uuid: new_uuid, address: new_address, first_name: first_name, last_name: last_name, password: password, vfe: vfe, privacy: privacy).perform}.to change(Workers::LegacyEmailTable::Rename.jobs, :size).by(1)
     end
   end
 
   context 'a new owner' do
     it 'deletes the email from the old owner in trogdir' do
-      expect { subject.new(email.id, new_uuid, new_address, first_name, last_name, password, vfe, privacy).perform}.to change(Workers::Trogdir::DeleteEmail.jobs, :size).by(1)
+      expect { subject.new(id: email.id, uuid: new_uuid, address: new_address, first_name: first_name, last_name: last_name, password: password, vfe: vfe, privacy: privacy).perform}.to change(Workers::Trogdir::DeleteEmail.jobs, :size).by(1)
     end
 
     it 'creates a new email under the new owner in trogdir when new_uuid present' do
-      expect { subject.new(email.id, new_uuid, new_address, first_name, last_name, password, vfe, privacy).perform}.to change(Workers::Trogdir::CreateEmail.jobs, :size).by(1)
+      expect { subject.new(id: email.id, uuid: new_uuid, address: new_address, first_name: first_name, last_name: last_name, password: password, vfe: vfe, privacy: privacy).perform}.to change(Workers::Trogdir::CreateEmail.jobs, :size).by(1)
     end
 
     it 'does not create a new email in trogdir if the new_uuid is not present' do
-      expect { subject.new(email.id, '', new_address, first_name, last_name, password, vfe, privacy).perform}.to change(Workers::Trogdir::CreateEmail.jobs, :size).by(0)
+      expect { subject.new(id: email.id, uuid: '', address: new_address, first_name: first_name, last_name: last_name, password: password, vfe: vfe, privacy: privacy).perform}.to change(Workers::Trogdir::CreateEmail.jobs, :size).by(0)
     end
 
     it 'updates the owner in the legacy table if the old and new uuid is present and different' do
-      expect { subject.new(email.id, new_uuid, new_address, first_name, last_name, password, vfe, privacy).perform}.to change(Workers::LegacyEmailTable::UpdateID.jobs, :size).by(1)
+      expect { subject.new(id: email.id, uuid: new_uuid, address: new_address, first_name: first_name, last_name: last_name, password: password, vfe: vfe, privacy: privacy).perform}.to change(Workers::LegacyEmailTable::UpdateID.jobs, :size).by(1)
     end
 
     it 'expires the email in the legacy table if the old uuid is present but the new uuid is not' do
-      expect { subject.new(email.id, '', new_address, first_name, last_name, password, vfe, privacy).perform}.to change(Workers::LegacyEmailTable::Expire.jobs, :size).by(1)
+      expect { subject.new(id: email.id, uuid: '', address: new_address, first_name: first_name, last_name: last_name, password: password, vfe: vfe, privacy: privacy).perform}.to change(Workers::LegacyEmailTable::Expire.jobs, :size).by(1)
     end
 
     it 'does not rename the email in the legacy table if the old uuid is present but the new uuid is not' do
-      expect { subject.new(email.id, '', new_address, first_name, last_name, password, vfe, privacy).perform}.to change(Workers::LegacyEmailTable::UpdateID.jobs, :size).by(0)
+      expect { subject.new(id: email.id, uuid: '', address: new_address, first_name: first_name, last_name: last_name, password: password, vfe: vfe, privacy: privacy).perform}.to change(Workers::LegacyEmailTable::UpdateID.jobs, :size).by(0)
     end
   end
 end
