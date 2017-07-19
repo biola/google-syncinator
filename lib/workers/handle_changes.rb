cff0ccf9a3119af25e42b03f6517ce5056140555
O. Bucao <orlando.bucao@biola.edu>
2017-07-19 16:15:38 -0700

    update paging on handle_changes to 100

--- lib/workers/handle_changes.rb
+++ lib/workers/handle_changes.rb
@@ -20,7 +20,7 @@ module Workers
 
       begin
         loop do
-          response = change_syncs.start(limit: 10).perform
+          response = change_syncs.start(limit: 100).perform
           break if response.parse.blank?
           raise TrogdirAPIError, response.parse['error'] unless response.success?
 
